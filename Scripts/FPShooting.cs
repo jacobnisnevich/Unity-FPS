52e2086178e5870868437bfe2c5c9db7d9e1c12d
jacobnisnevich <jacob.nisnevich@gmail.com>
2014-08-27 20:25:18 -0700

    initial commit

---
+++ Scripts/FPShooting.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPShooting : MonoBehaviour {
+
+	public GameObject bullet_prefab;
+	public Texture2D crosshairImage;
+	float bulletImpulse = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			Camera cam = transform.FindChild ("Main Camera").camera;
+			GameObject theBullet = (GameObject)Instantiate(bullet_prefab, cam.transform.position, cam.transform.rotation);
+			theBullet.rigidbody.AddForce(cam.transform.forward * bulletImpulse, ForceMode.Impulse);
+		}
+	}
+
+	void OnGUI()
+	{
+		float xMin = (Screen.width / 2) - (crosshairImage.width / 2);
+		float yMin = (Screen.height / 2) - (crosshairImage.height / 2);
+		GUI.DrawTexture(new Rect(xMin, yMin, crosshairImage.width, crosshairImage.height), crosshairImage);
+	}
+}
